774c77755c034d3ac81c90fdbd9b909336af0193
Alexander Tipugin <atipugin@gmail.com>
2016-01-15 20:27:18 +0300

    Add if/unless options for handlers

--- lib/eventador/jobs/handle_event_job.rb
+++ lib/eventador/jobs/handle_event_job.rb
@@ -3,8 +3,32 @@ module Eventador
     class HandleEventJob < BaseJob
       def perform(params_dump)
         params = YAML.load(params_dump)
+        handler = params[:handler]
         event = Eventador.store.find_event(params[:event_id])
-        params[:handler][:klass].new.run(event)
+
+        return unless conditions_satisfied?(handler, event)
+
+        handler[:klass].new.run(event)
+      end
+
+      private
+
+      def conditions_satisfied?(handler, event)
+        subsequent_events =
+          Eventador.store.find_subsequent_events(event).map { |e| e['name'] }
+
+        if_satisfied?(handler, subsequent_events) &&
+          unless_satisfied?(handler, subsequent_events)
+      end
+
+      def if_satisfied?(handler, subsequent_events)
+        return true unless handler[:if]
+        subsequent_events.include?(handler[:if].to_s)
+      end
+
+      def unless_satisfied?(handler, subsequent_events)
+        return true unless handler[:unless]
+        !subsequent_events.include?(handler[:unless].to_s)
       end
     end
   end
--- spec/lib/eventador/jobs/handle_event_job_spec.rb
+++ spec/lib/eventador/jobs/handle_event_job_spec.rb
@@ -7,20 +7,74 @@ module Eventador
       let(:event_id) do
         Eventador.store.save_event(event_name, event_properties, event_options)
       end
-      let(:params_dump) do
-        YAML.dump(
-          event_id: event_id,
-          handler: Eventador.registry[event_name][0]
-        )
+      let(:handler) { Eventador.registry[event_name][0] }
+      let(:params_dump) { YAML.dump(event_id: event_id, handler: handler) }
+      let(:subsequent_events) do
+        Eventador.store.find_subsequent_events(event).map { |e| e['name'] }
       end
 
       before do
-        handler_klass.handle event_name
+        handler_klass.handle event_name, handler_options
       end
 
       describe '#perform' do
         it 'runs handler' do
           # ...
+        end
+      end
+
+      describe '#conditions_satisfied?' do
+        it 'returns true' do
+          expect(subject.send(:conditions_satisfied?, handler, event))
+            .to be(true)
+        end
+      end
+
+      describe '#if_satisfied?' do
+        let(:handler_options) { { if: FFaker::Lorem.word } }
+
+        it 'returns false' do
+          expect(subject.send(:if_satisfied?, handler, subsequent_events))
+            .to be(false)
+        end
+
+        context 'when expected event is occurred' do
+          before do
+            Eventador.store.save_event(
+              handler_options[:if],
+              event_properties,
+              event_options.merge(occurred_at: 1.minute.from_now)
+            )
+          end
+
+          it 'returns true' do
+            expect(subject.send(:if_satisfied?, handler, subsequent_events))
+              .to be(true)
+          end
+        end
+      end
+
+      describe '#unless_satisfied?' do
+        let(:handler_options) { { unless: FFaker::Lorem.word } }
+
+        it 'returns true' do
+          expect(subject.send(:unless_satisfied?, handler, subsequent_events))
+            .to be(true)
+        end
+
+        context 'when unexpected event is occurred' do
+          before do
+            Eventador.store.save_event(
+              handler_options[:unless],
+              event_properties,
+              event_options.merge(occurred_at: 1.minute.from_now)
+            )
+          end
+
+          it 'returns false' do
+            expect(subject.send(:unless_satisfied?, handler, subsequent_events))
+              .to be(false)
+          end
         end
       end
     end
