3b04cd46f52613a9d74aba2b2c97647d6d601834
Alexander Tipugin <atipugin@gmail.com>
2016-01-19 20:04:57 +0300

    Improve handler specs

--- spec/lib/streamline/jobs/handle_event_job_spec.rb
+++ spec/lib/streamline/jobs/handle_event_job_spec.rb
@@ -7,6 +7,7 @@ module Streamline
       let(:handler) { Streamline.registry[event_name].sample }
       let(:params) { { event_id: event_id, handler: handler } }
       let(:params_dump) { YAML.dump(params) }
+      let(:subsequent_events) { Streamline.store.find_subsequent_events(event) }
 
       before do
         handler_class.handle(event_name, handler_options)
@@ -16,6 +17,88 @@ module Streamline
         it 'runs handler' do
           allow_any_instance_of(handler_class).to receive(:run).with(event)
           subject.perform(params_dump)
+        end
+      end
+
+      describe '#conditions_satisfied?' do
+        it 'returns true' do
+          expect(subject.send(:conditions_satisfied?, handler, event))
+            .to be(true)
+        end
+
+        context 'when handler conditions are not satisfied' do
+          let(:handler_options) { { if: random_word } }
+
+          it 'returns false' do
+            expect(subject.send(:conditions_satisfied?, handler, event))
+              .to be(false)
+          end
+        end
+      end
+
+      describe '#if_satisfied?' do
+        let(:handler_options) { { if: random_word } }
+
+        it 'returns false' do
+          expect(subject.send(:if_satisfied?, handler, subsequent_events))
+            .to be(false)
+        end
+
+        context 'when expected event is occurred' do
+          before do
+            Streamline.store.save_event(
+              event_attributes.merge(
+                name: handler_options[:if],
+                occurred_at: 1.hour.from_now
+              )
+            )
+          end
+
+          it 'returns true' do
+            expect(subject.send(:if_satisfied?, handler, subsequent_events))
+              .to be(true)
+          end
+        end
+      end
+
+      describe '#unless_satisfied?' do
+        let(:handler_options) { { unless: random_word } }
+
+        it 'returns true' do
+          expect(subject.send(:unless_satisfied?, handler, subsequent_events))
+            .to be(true)
+        end
+
+        context 'when unexpected event is occurred' do
+          before do
+            Streamline.store.save_event(
+              event_attributes.merge(
+                name: handler_options[:unless],
+                occurred_at: 1.hour.from_now
+              )
+            )
+          end
+
+          it 'returns false' do
+            expect(subject.send(:unless_satisfied?, handler, subsequent_events))
+              .to be(false)
+          end
+        end
+      end
+
+      describe '#enqueue_repeat' do
+        it 'does not enqueue another repeat' do
+          expect { subject.send(:enqueue_repeat, handler, params) }
+            .not_to enqueue_a(described_class)
+        end
+
+        describe 'when amount of repeats greater than one' do
+          let(:handler_options) { { repeats: 2 } }
+
+          it 'enqueues another repeat' do
+            expect { subject.send(:enqueue_repeat, handler, params) }
+              .to enqueue_a(described_class)
+          end
         end
       end
     end
--- spec/support/store_examples.rb
+++ spec/support/store_examples.rb
@@ -40,7 +40,7 @@ RSpec.shared_examples_for 'a store' do
   describe '#find_subsequent_events' do
     before do
       Streamline.store.save_event(
-        event_attributes.merge(occurred_at: 1.minute.from_now)
+        event_attributes.merge(occurred_at: 1.hour.from_now)
       )
     end
 
